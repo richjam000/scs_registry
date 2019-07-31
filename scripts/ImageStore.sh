70fe47a81ebc02a18a31d503f8cbc6332264383e
Richard Jamieson <richard.jamieson@scsuk.net>
2019-07-31 15:37:38 +0100

    Updated to push to gitlab registry

--- scripts/ImageStore.sh
+++ scripts/ImageStore.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-INTERNAL_REGISTRY=localhost:5000
+GITLAB_REGISTRY=gitlab.scsuk.net:5005
+docker login "$GITLAB_REGISTRY"
+echo '##################################################################'
+echo "Pulling external images and storing them locally at $GITLAB_REGISTRY"
+echo '------------------------------------'
+
+GITLAB_REGISTRY_PROJECT="$GITLAB_REGISTRY"/scsuk/ext_registry
 grep -v ^# ./ImageStore.conf | while read DETAIL ; do
    SUPPLIER=$(echo "$DETAIL" | cut -d '#' -f 1)
    EXTERNAL_IMAGE=$(echo "$DETAIL" | cut -d '#' -f 2)
@@ -7,13 +13,21 @@ grep -v ^# ./ImageStore.conf | while read DETAIL ; do
    INTERNAL_TAG=$(echo "$DETAIL" | cut -d '#' -f 4)
    INTERNAL_TAG=${INTERNAL_TAG:-$EXTERNAL_TAG} # if blank will be same as external tag
    IMAGE_NAME=$(echo "$EXTERNAL_IMAGE" | awk -F/ '{print $NF}')
+   echo "PULLING: $EXTERNAL_IMAGE:$EXTERNAL_TAG"
+   set -x
    docker pull "$EXTERNAL_IMAGE:$EXTERNAL_TAG"
+   set +x
    IMAGE_ID=$(docker image ls "$EXTERNAL_IMAGE:$EXTERNAL_TAG" -q)
-   docker tag $IMAGE_ID "$INTERNAL_REGISTRY/$SUPPLIER/$IMAGE_NAME:$INTERNAL_TAG"
-   docker push "$INTERNAL_REGISTRY/$SUPPLIER/$IMAGE_NAME:$INTERNAL_TAG"
-   curl -s "http://$INTERNAL_REGISTRY/v2/$SUPPLIER/$IMAGE_NAME/tags/list" | python -m json.tool
+   echo "ADD TAG: $GITLAB_REGISTRY_PROJECT/$SUPPLIER/$IMAGE_NAME:$INTERNAL_TAG"
+   set -x
+   docker tag $IMAGE_ID "$GITLAB_REGISTRY_PROJECT/$SUPPLIER/$IMAGE_NAME:$INTERNAL_TAG"
+   set +x
+   echo "PUSH TO: $GITLAB_REGISTRY_PROJECT/$SUPPLIER/$IMAGE_NAME:$INTERNAL_TAG"
+   set -x
+   docker push "$GITLAB_REGISTRY_PROJECT/$SUPPLIER/$IMAGE_NAME:$INTERNAL_TAG"
+   set +x
+   echo '------------------------------------'
 done 
 
-echo '------------------------------------'
-echo "List INTERNAL_REGISTRY $INTERNAL_REGISTRY"
-curl -s http://"$INTERNAL_REGISTRY"/v2/_catalog | python -m json.tool
+echo "See: https://gitlab.scsuk.net/scsuk/scs_registry/container_registry"
+echo '##################################################################'
